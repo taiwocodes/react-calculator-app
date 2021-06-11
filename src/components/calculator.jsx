4474be1fc0f5598fd6d038f11d87b15cae51178b
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2021-06-11 11:08:10 +0000

    added delete button

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -19,6 +19,10 @@ function Calculator() {
 		setText(prev => prev + e.target.value);
 	};
 
+	const del = () => {
+		setText(prev => prev.slice(0, prev.length - 1));
+	};
+
 	const actionHandler = e => {
 		const action = e.target.value;
 
@@ -43,8 +47,24 @@ function Calculator() {
 		<div>
 			<div className='calc_layout'>
 				<Output text={text} />
-				<button onClick={clear} className='span-all clear'>
+				<button onClick={clear} className='span-3 clear'>
 					Clear
+				</button>
+				<button onClick={del} className=''>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						className='icon'
+						fill='none'
+						viewBox='0 0 24 24'
+						stroke='currentColor'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth={2}
+							d='M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z'
+						/>
+					</svg>
 				</button>
 				<Button value='7' handler={updateText} />
 				<Button value='8' handler={updateText} />
